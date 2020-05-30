70d4f199f04917fa144d26fb78724b86f408de93
Brandon Tom <magicink@gmail.com>
2020-05-30 09:11:03 -0700

    Initial commit

---
+++ Assets/Scripts/Systems/MovableSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MovableSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float dt = Time.DeltaTime;
+        
+        Entities.ForEach((in Movable movable) =>
+        {
+            var step = movable.direction * movable.speed;
+        }).Schedule();
+    }
+}
